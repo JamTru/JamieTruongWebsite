a989410bd943e3e42c9137637852541ce473a85c
JamTru <truongjamie4@gmail.com>
2024-02-08 17:54:53 +1100

    Added Images Figured out that I can't import from public Added attribution and background image styling

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,8 +1,12 @@
 import AnimatedText from './AnimatedText.js';
+import './Profile.css';
 const Profile = () => {
   return (
     <div id="profile">
-      <AnimatedText text="Hello, world. My name is" delay={75} />
+      <div className="textPositioning">
+        <AnimatedText text="Hello, world. My name is" delay={75} />
+        <h1>Jamie Truong</h1>
+      </div>
     </div>
   );
 };
