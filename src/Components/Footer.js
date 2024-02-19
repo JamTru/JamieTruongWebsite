7c280a8eb28ee320010ca30eecdaf4326314a6f7
JamTru <truongjamie4@gmail.com>
2024-02-19 17:18:31 +1100

    I am so confused
    ??

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <a href="https://www.flaticon.com/free-icons/cyberpunk" title="cyberpunk icons">Cyberpunk icons created by Freepik - Flaticon</a>
+      <a href="https://www.vecteezy.com/free-vector/abstract">Abstract Vectors by Vecteezy</a>
+    </div>
+  );
+};
+export default Footer;
