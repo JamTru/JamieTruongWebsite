f0a22d0cc7d2d4c84908b137b34c6ff79ff3b00a
JamTru <truongjamie4@gmail.com>
2024-02-19 16:41:59 +1100

    Fleshed out Portfolio section of website Added Video Content to Portfolio Added Code to Package to Host on Github

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,16 @@
 import './Header.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
       <Navbar expand="lg" className="navBarStyle">
         <Navbar.Brand>
-          <a href="#profile"><img src="/hologram.png" alt="Hologram Projector Icon" className="icon"></img></a>
+        {/*Anchor here has rel tag to avoid the possibility of tabnapping on my page.*/}
+          <a href="https://github.com/JamTru/JamieTruongWebsite" target="_blank" rel="noopener noreferrer">
+            <img src="/hologram.png" alt="Hologram Projector Icon" className="icon"></img>
+          </a>
         </Navbar.Brand>
         <Nav className="me-auto">
           <a href="#portfolio">Portfolio</a>
